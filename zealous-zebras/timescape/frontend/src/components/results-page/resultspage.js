2435642d7b5fbec3fcef6203dc6ee18a21a5747d
Gustav Odinger <gustav@odinger.se>
2020-08-09 18:28:44 +0200

    Hooked up Results page to use API

--- zealous-zebras/timescape/frontend/src/components/results-page/resultspage.js
+++ zealous-zebras/timescape/frontend/src/components/results-page/resultspage.js
@@ -7,55 +7,40 @@ let current_year = "1998";
 
 // Imports the correct style sheet.
 (async () => {
-	await import(`./styling/${current_year}.css`);
+  await import(`./styling/${current_year}.css`);
 })();
 
 class ResultsPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {response: []};
+  constructor(props) {
+    super(props);
+    this.state = { response: [] };
   }
-  
-	componentDidMount() {
-		axios
-			.get(`http://localhost:8000/api/v1.0/search/?query=${this.props.query}&page=1`)
-			.then((response) => {
-        response = [
-          {
-              "id": "5GViT37xTNoJ",
-              "title": "Dog Breeds - Types Of Dogs - American Kennel Club",
-              "htmlTitle": "<b>Dog</b> Breeds - Types Of <b>Dogs</b> - American Kennel Club",
-              "link": "https://www.akc.org/dog-breeds/",
-              "snippet": "What's a dog breed? People have been breeding dogs since prehistoric times. \nThe earliest dog breeders used wolves to create domestic dogs. From the ..."
-          },
-          {
-            "id": "5GViT37xTNoJ",
-            "title": "Dog Breeds - Types Of Dogs - American Kennel Club",
-            "htmlTitle": "<b>Dog</b> Breeds - Types Of <b>Dogs</b> - American Kennel Club",
-            "link": "https://www.akc.org/dog-breeds/",
-            "snippet": "What's a dog breed? People have been breeding dogs since prehistoric times. \nThe earliest dog breeders used wolves to create domestic dogs. From the ..."
-        }
-      ]
-      this.setState({response: response});
-      });
-	}
 
-	render() {
-		return (
-			<div>
-				<div>
+  componentDidMount() {
+    axios
+      .get(`http://localhost:8000/api/v1.0/search/?query=${this.props.query}&page=1`)
+      .then((response) => {
+        console.log(response);
+        this.setState({ response: response.data });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
           {
-          this.state.response.map((result)=>{
-            return <Card title={{__html: result.htmlTitle}}
-            description={{__html: result.snippet}}
-            link={result.link}
-            />
-          })}
-				</div>
-				<Navigation />
-			</div>
-		);
-	}
+            this.state.response.map((result) => {
+              return <Card title={{ __html: result.htmlTitle }}
+                description={{ __html: result.snippet }}
+                link={result.link}
+              />
+            })}
+        </div>
+        <Navigation />
+      </div>
+    );
+  }
 }
 
 export default ResultsPage;
